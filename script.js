21dc2cb8fbe500e02da8627e67ae098a2172dd28
Scott Johnson <scottj@thegrovery.com>
2024-07-25 10:15:53 -0400

    fixes for mobile

--- script.js
+++ script.js
@@ -7,24 +7,24 @@ document.addEventListener('DOMContentLoaded', () => {
     path.style.strokeDasharray = pathLength;
     path.style.strokeDashoffset = pathLength;
 
-    // Do not hide the path initially
-    // path.classList.add('hidden');
+    // Hide the path initially
+    path.classList.add('hidden');
 
     // Add touch event listener
     videoWrapper.addEventListener('touchstart', () => {
         backgroundVideo.src += "&autoplay=1&mute=1";
     });
 
-    document.addEventListener('scroll', () => {
+    window.addEventListener('scroll', () => {
         // Reveal the path on the first scroll event
         if (path.classList.contains('hidden')) {
             path.classList.remove('hidden');
         }
 
-        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        const docHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        const scrollPercentage = scrollTop / docHeight;
+        var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+        var draw = pathLength * scrollpercent;
 
-        path.style.strokeDashoffset = pathLength * (1 - scrollPercentage);
+        // Reverse the drawing (when scrolling upwards)
+        path.style.strokeDashoffset = pathLength - draw;
     });
 });
